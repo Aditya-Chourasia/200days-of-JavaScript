1f653260fed215961bd3431b982613a0924e0651
Aditya Chaurasia <chaurasiaaditya102@gmail.com>
2022-12-11 11:33:29 +0000

    JS BASICS ENDS HERE

--- index.js
+++ index.js
@@ -510,3 +510,21 @@ function findGreater(a, b) {
 }
 
 // Use Multiple Conditional (Ternary) Operators
+function findGreaterOrEqual(a, b) {
+  if (a === b) {
+    return "a and b are equal";
+  }
+  else if (a > b) {
+    return "a is greater";
+  }
+  else {
+    return "b is greater";
+  }
+}
+
+
+function findGreaterOrEqual(a, b) {
+  return (a === b) ? "a and b are equal" 
+    : (a > b) ? "a is greater" 
+    : "b is greater";
+}
